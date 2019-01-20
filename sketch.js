8ee298422d1242603f18ca7d7e39e3a80dfe327b
lepittaway <lornapittaway@hotmail.com>
2019-01-20 22:03:23 +0000

    white

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-var alph, beta, gamma;
+var alpha, beta, gamma;
 var xpos, ypos;
 
 var heya;
@@ -7,10 +7,6 @@ function setup()
 {
   // set canvas size
   createCanvas(windowWidth, windowHeight);
-  
-  /*heya = createImg('assets/hills.jpg');
-  
-  heya.style('width', '100px');*/
   
   // default values
   xpos = 200;
@@ -22,40 +18,34 @@ function draw() {
   background(10);
   
   
-  alph = 0;
-  beta = 0;
-  gamma = 0;
+  var alpha = 0;
+var beta = 0;
+var gamma = 0;
   
-  //heya = document.getElementsByClassName("hills")[0]; 
+  heya = document.getElementsByClassName("hills"); 
 
   // display variables
+  fill(100);
+  noStroke();
+  text("alpha: " + alpha, 25, 25);
+  text("gamma: " + gamma, 25, 50);
   
-  
-  var a = constrain(alph, 80, 110);
+  var a = constrain(alpha, 80, 110);
   var a2 = map(a, 80, 110, 40, windowWidth - 40);
   
   var g = constrain(gamma, -70, -20);
   var g2 = map(g, -70, -20, windowHeight - 40, 40);
-  
-  //heya.style('left', a2 + 'px');
-  //heya.style('top', g2 + 'px');
   
   push();
   angleMode(DEGREES);
   rectMode(RADIUS);
   //translate(200, 200);
   //rotate(a2);
-  fill(0, 0, 255);
-  //heya.position(a2, g2);
+  fill(10);
   rect(a2, g2, 40, 40);
   fill(0);
   //rect(-40, 40, 80, 5);
   pop();	
-  
-  fill(100);
-  noStroke();
-  text("alpha: " + alph, 25, 25);
-  text("gamma: " + gamma, 25, 50);
 }
 
 // accelerometer Data
@@ -65,7 +55,7 @@ var gn = new GyroNorm();
 gn.init().then(function() {
 	supported = true;
 	gn.start(function(event) {
-		alph = event.do.alpha;
+		alpha = event.do.alpha;
 		beta = event.do.beta;
 		gamma = event.do.gamma;
 	});
