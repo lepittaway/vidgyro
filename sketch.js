2a93969fbb9512f20f2d2421078c8f6cf9b3d854
lepittaway <lornapittaway@hotmail.com>
2019-01-20 21:50:35 +0000

    image test

--- sketch.js
+++ sketch.js
@@ -1,22 +1,32 @@
-var alpha = 0;
-var beta = 0;
-var gamma = 0;
+var alpha, beta, gamma;
 var xpos, ypos;
+
+var heya;
 
 function setup() 
 {
   // set canvas size
   createCanvas(windowWidth, windowHeight);
   
+  heya = createImg('assets/hills.jpg');
+  
+  heya.style('width', '100px');
+  
   // default values
   xpos = 200;
   ypos = 200;
 }
 
-function draw() 
-{
+function draw() {
   // set background color to white
   background(10);
+  
+  
+  alpha = 0;
+  beta = 0;
+  gamma = 0;
+  
+  //heya = document.getElementsByClassName("hills")[0]; 
 
   // display variables
   fill(100);
@@ -30,12 +40,16 @@ function draw()
   var g = constrain(gamma, -70, -20);
   var g2 = map(g, -70, -20, windowHeight - 40, 40);
   
+  //heya.style('left', a2 + 'px');
+  //heya.style('top', g2 + 'px');
+  
   push();
   angleMode(DEGREES);
   rectMode(RADIUS);
   //translate(200, 200);
   //rotate(a2);
   fill(0, 0, 255);
+  heya.position(a2, g2);
   rect(a2, g2, 40, 40);
   fill(0);
   //rect(-40, 40, 80, 5);
