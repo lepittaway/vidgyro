945cdb3c243c9ac50582c4b4bf3f86693c53fa83
lepittaway <lornapittaway@hotmail.com>
2019-01-20 22:18:16 +0000

    red

--- sketch.js
+++ sketch.js
@@ -1,69 +1,46 @@
 var alpha, beta, gamma;
 var xpos, ypos;
 
-var heya;
-
 function setup() 
 {
   // set canvas size
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(400, 400);
   
   // default values
   xpos = 200;
   ypos = 200;
+  alpha = 0;
+  beta = 0;
+  gamma = 0;
 }
 
-function draw() {
+function draw() 
+{
   // set background color to white
-  background(10);
-  
-  
-  var alpha = 0;
-var beta = 0;
-var gamma = 0;
-  
-  //heya = document.getElementsByClassName("hills"); 
+  background(255);
 
   // display variables
-  
+  fill(100);
   noStroke();
+  text("alpha: " + alpha, 25, 25);
+  text("beta: " + beta, 25, 50);
+  fill(255, 0, 0);
+  text("gamma: " + gamma, 25, 75);
   
   push();
-  var a = constrain(alpha, 70, 120);
-  var a2 = map(a, 70, 120, 40, windowWidth - 40);
-  
-  var g = constrain(gamma, -89, -1);
-  var g2 = map(g, -89, -1, windowHeight - 40, 40);
-  
-  
-  
-  angleMode(DEGREES);
-  rectMode(RADIUS);
-  //translate(200, 200);
-  //rotate(a2);
-  fill(250);
-  rect(a2, g2, 40, 40);
+  translate(200, 200);
+  rotate(radians(gamma));
+  fill(255, 0, 0);
+  rect(-40, -40, 80, 80);
   fill(0);
-  //rect(-40, 40, 80, 5);
+  rect(-40, 40, 80, 5);
   pop();	
-  
-  fill('red');
-  text("alpha: " + alpha, 25, 25);
-  text("gamma: " + gamma, 25, 50);
 }
 
 // accelerometer Data
-
-var gn = new GyroNorm();
-
-gn.init().then(function() {
-	supported = true;
-	gn.start(function(event) {
-		alpha = event.do.alpha;
-		beta = event.do.beta;
-		gamma = event.do.gamma;
-	});
-}).catch(function(e) {
-  // Catch if the DeviceOrientation or DeviceMotion is not supported by the browser or device
-	// Do stuff here when device does not have a gyro
+window.addEventListener('deviceorientation', function(e) 
+{
+  alpha = e.alpha;
+  beta = e.beta;
+  gamma = e.gamma;
 });
